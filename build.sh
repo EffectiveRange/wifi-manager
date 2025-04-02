7432df7ed1aeec4ade10206985e7d6cd46f21c0e
Attila Gombos <attila.gombos@effective-range.com>
2025-04-02 11:18:56 +0200

    Add dependabot config and build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+WORKSPACE="/workspaces/wifi-manager"
+BUILDROOT="/var/chroot/buildroot"
+
+sudo rsync -av --exclude ".git" --mkpath "${WORKSPACE}/" "${BUILDROOT}${WORKSPACE}/"
+
+sudo schroot -p -c buildroot -- apt update
+sudo schroot -p -c buildroot -- apt install -y python3-stdeb packaging-tools
+sudo schroot -p -c buildroot -- pack_python . -s dh-virtualenv
+
+sudo rsync -av --include "*" --mkpath "${BUILDROOT}${WORKSPACE}/dist/" "${WORKSPACE}/dist/"
