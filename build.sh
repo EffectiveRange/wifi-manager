e737ed3be27df0f188e84f02de5f09ab621c57ac
AttilaGombosER <attila.gombos@effective-range.com>
2025-04-24 11:38:26 +0200

    Init config file after install (#25)

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 WORKSPACE="/workspaces/wifi-manager"
 BUILDROOT="/var/chroot/buildroot"
 
+sudo rm -rf dist build *.egg-info
+
 sudo rsync -av --exclude ".git" --exclude ".*cache" --mkpath "${WORKSPACE}/" "${BUILDROOT}${WORKSPACE}/"
 
 sudo schroot -p -c buildroot -- apt update
 sudo schroot -p -c buildroot -- apt install -y python3-stdeb packaging-tools
+sudo schroot -p -c buildroot -- rm -rf dist build *.egg-info
 sudo schroot -p -c buildroot -- pack_python . -s dh-virtualenv
 
 sudo rsync -av --include "*" --mkpath "${BUILDROOT}${WORKSPACE}/dist/" "${WORKSPACE}/dist/"
