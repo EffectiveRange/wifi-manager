4ba0d31e0770ca05d073dd461b4f9fe75f192374
Attila Gombos <attila.gombos@effective-range.com>
2026-04-18 20:05:07 +0200

    Update config handling

--- bin/wifi-manager.py
+++ bin/wifi-manager.py
@@ -4,10 +4,9 @@
 # SPDX-FileCopyrightText: 2024 Attila Gombos <attila.gombos@effective-range.com>
 # SPDX-License-Identifier: MIT
 
-import pathlib
-
 import gi
 
+from arguments import APPLICATION_NAME, get_argument_parser
 from wifi_connection import (
     ConnectionMonitorConfig,
     ConnectionMonitor,
@@ -15,9 +14,8 @@ from wifi_connection import (
 )
 from wifi_dbus import WpaSupplicantDbus, NetworkManagerDbus
 
-gi.require_version("NM", "1.0")
+gi.require_version('NM', '1.0')
 import os
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from pathlib import Path
 from signal import signal, SIGINT, SIGTERM
 from threading import Thread
@@ -66,98 +64,60 @@ from wifi_utility import (
 )
 from wifi_config import WpaSupplicantConfig, NetworkManagerConfig
 
-APPLICATION_NAME = "wifi-manager"
+log = get_logger('WifiManagerApp')
 
-log = get_logger("WifiManagerApp")
+DEFAULT_CONFIG_PATH = Path(f'/etc/effective-range/{APPLICATION_NAME}/{APPLICATION_NAME}.conf.default')
 
 
 def main() -> None:
-    resource_root = _get_resource_root()
-    arguments = _get_arguments()
-
     setup_logging(APPLICATION_NAME)
 
-    log.info(f"Started {APPLICATION_NAME}", arguments=arguments)
+    resource_root = _get_resource_root()
+    argument_parser = get_argument_parser()
 
-    config = ConfigLoader(Path(f"{resource_root}/config/{APPLICATION_NAME}.conf.default")).load(arguments)
+    config = ConfigLoader(DEFAULT_CONFIG_PATH).load(argument_parser)
 
-    _update_logging(arguments, config)
-
-    log.info("Retrieved configuration", configuration=config)
-
-    try:
-        config_dir = Path(config["config_file"]).parent
-        api_server_port = int(config.get("api_server_port", 8080))
-        device_role = config.get("device_role", "edge")
-        device_hostname = config.get("device_hostname", "er-{{device_role}}-{{cpu_serial}}")
-        wlan_interface = config.get("wlan_interface", "wlan0")
-        wlan_country = config.get("wlan_country", "HU")
-        disable_power_save = config.get("wlan_disable_power_save", False)
-        disable_roaming = config.get("wlan_disable_roaming", False)
-        switch_fail_limit = int(config.get("control_switch_fail_limit", 5))
-        switch_fail_command = config.get("control_switch_fail_command", "reboot")
-        client_timeout = int(config.get("client_timeout", 15))
-        client_restart_delay = int(config.get("client_restart_delay", 5))
-        hotspot_password = config.get("hotspot_password", "p4ssw0rd")
-        hotspot_peer_timeout = int(config.get("hotspot_peer_timeout", 120))
-        hotspot_static_ip = config.get("hotspot_static_ip", "192.168.100.1")
-        hotspot_dhcp_range = config.get("hotspot_dhcp_range", "192.168.100.2,192.168.100.254,255.255.255.0,2m")
-        hotspot_startup_delay = int(config.get("hotspot_startup_delay", 5))
-        connection_ping_interval = int(config.get("connection_ping_interval", 60))
-        connection_ping_timeout = int(config.get("connection_ping_timeout", 5))
-        connection_ping_fail_limit = int(config.get("connection_ping_fail_limit", 5))
-        connection_connect_actions = config.get("connection_connect_actions", "").strip().split("\n")
-        connection_restore_actions = config.get("connection_restore_actions", "reset-wireless").strip().split("\n")
-        identify_pin_gpio_number = int(config.get("identify_pin_gpio_number", 12))
-        identify_pin_active_high = config.get("identify_pin_active_high", True)
-        identify_pin_initial_value = config.get("identify_pin_initial_value", False)
-        identify_blink_frequency = float(config.get("identify_blink_frequency", 440))
-        identify_blink_interval = float(config.get("identify_blink_interval", 0.5))
-        identify_blink_pause = float(config.get("identify_blink_pause", 0.5))
-        identify_blink_count = int(config.get("identify_blink_count", 3))
-        command_definitions = config.get("command_definitions", "").strip().split("\n")
-    except KeyError as error:
-        raise ValueError(f"Missing configuration key: {error}")
+    setup_logging(APPLICATION_NAME, config.log_level, config.log_file, warn_on_overwrite=False)
 
     platform = PlatformAccess()
-    wlan_interface = WlanInterfaceSelector(platform).select(wlan_interface)
+    wlan_interface = WlanInterfaceSelector(platform).select(config.wlan_interface)
     debian_12_or_higher = platform.get_platform_version() >= 12.0
     platform_config = PlatformConfig(platform, wlan_interface,
-                                     "/boot/firmware/config.txt" if debian_12_or_higher else "/boot/config.txt")
+                                     '/boot/firmware/config.txt' if debian_12_or_higher else '/boot/config.txt')
 
-    if platform_config.setup(disable_power_save, disable_roaming):
-        log.warning("Platform configuration changed, reboot to apply changes")
+    if platform_config.setup(config.wlan_disable_power_save, config.wlan_disable_roaming):
+        log.warning('Platform configuration changed, reboot to apply changes')
 
     cpu_serial = platform.get_cpu_serial()
     mac_address = platform.get_mac_address(wlan_interface)
 
     id_context = {
-        "device_role": device_role,
-        "cpu_serial": cpu_serial,
-        "mac_address": mac_address,
+        'device_role': config.device_role,
+        'cpu_serial': cpu_serial,
+        'mac_address': mac_address,
     }
 
-    hostname = Template(device_hostname).render(id_context)
-    id_context["hostname"] = hostname
+    hostname = Template(config.device_hostname).render(id_context)
+    id_context['hostname'] = hostname
 
     system_bus = SystemBus(DBusGMainLoop(set_as_default=True))
 
     with SystemdDbus(system_bus) as systemd:
-        wpa_config = WpaSupplicantConfig(wlan_country)
+        wpa_config = WpaSupplicantConfig(config.wlan_country)
         wpa_dbus = WpaSupplicantDbus(wlan_interface, system_bus)
         nm_client = NM.Client.new(None)
-        nm_config = NetworkManagerConfig(wlan_interface, wlan_country)
+        nm_config = NetworkManagerConfig(wlan_interface, config.wlan_country)
         nm_dbus = NetworkManagerDbus(wlan_interface, nm_client)
         dnsmasq_config = DnsmasqConfig(
-            wlan_interface, hotspot_static_ip, hotspot_dhcp_range, api_server_port
+            wlan_interface, config.hotspot_static_ip, config.hotspot_dhcp_range, config.server_port
         )
         hostapd_config = HostapdConfig(
             wlan_interface,
             mac_address,
             hostname,
-            hotspot_password,
-            wlan_country,
-            hotspot_startup_delay,
+            config.hotspot_password,
+            config.wlan_country,
+            config.hotspot_startup_delay,
         )
         reader = JournalReader()
         journal = ServiceJournal(reader)
@@ -170,7 +130,7 @@ def main() -> None:
         dhcpcd_service = DhcpcdService(service_dependencies, system_bus, wlan_interface)
         avahi_service = AvahiService(service_dependencies, hostname)
         network_manager_service = NetworkManagerService(
-            service_dependencies, nm_config, nm_dbus, client_restart_delay
+            service_dependencies, nm_config, nm_dbus, config.client_restart_delay
         )
         dnsmasq_service = DnsmasqService(
             service_dependencies, system_bus, dnsmasq_config, resource_root
@@ -208,17 +168,20 @@ def main() -> None:
         )
 
         connection_monitor_timer = ReusableTimer()
+        connection_connect_actions = config.connection_connect_actions.strip().split('\n')
+        connection_restore_actions = config.connection_restore_actions.strip().split('\n')
         connect_actions = ConnectionAction.create_actions(
             connection_connect_actions, wifi_client_service, systemd, platform
         )
         restore_actions = ConnectionAction.create_actions(
             connection_restore_actions, wifi_client_service, systemd, platform
         )
+        config_dir = Path(config.config).parent
         connection_monitor_config = ConnectionMonitorConfig(
             config_dir,
-            connection_ping_interval,
-            connection_ping_timeout,
-            connection_ping_fail_limit,
+            config.connection_ping_interval,
+            config.connection_ping_timeout,
+            config.connection_ping_fail_limit,
             list(connect_actions),
             list(restore_actions)
         )
@@ -226,13 +189,15 @@ def main() -> None:
         connection_monitor = ConnectionMonitor(
             platform, systemd, connection_monitor_timer, connection_monitor_config
         )
-        wifi_control_config = WifiControlConfig(switch_fail_limit, switch_fail_command)
+        wifi_control_config = WifiControlConfig(config.control_switch_fail_limit, config.control_switch_fail_command)
         wifi_control = WifiControl(wifi_client_service, wifi_hotspot_service, platform, wifi_control_config)
         blink_config = BlinkConfig(
-            identify_blink_frequency, identify_blink_interval, identify_blink_pause, identify_blink_count
+            config.identify_blink_frequency, config.identify_blink_interval, config.identify_blink_pause,
+            config.identify_blink_count
         )
         blink_device = GpioBlinkDevice(
-            identify_pin_gpio_number, active_high=identify_pin_active_high, initial_value=identify_pin_initial_value
+            config.identify_pin_gpio_number, active_high=config.identify_pin_active_high,
+            initial_value=config.identify_pin_initial_value
         )
         blink_control = BlinkControl(blink_config, blink_device)
         event_handler_timer = ReusableTimer()
@@ -241,11 +206,12 @@ def main() -> None:
             blink_control,
             event_handler_timer,
             connection_monitor,
-            client_timeout,
-            hotspot_peer_timeout,
+            config.client_timeout,
+            config.hotspot_peer_timeout,
         )
+        command_definitions = config.command_definitions.strip().split('\n')
         web_server_config = WebServerConfig(
-            hotspot_static_ip, api_server_port, resource_root
+            config.hotspot_static_ip, config.server_port, resource_root
         )
         web_server = WifiWebServer(web_server_config, platform, event_handler, command_definitions)
 
@@ -257,7 +223,7 @@ def main() -> None:
         event_thread = Thread(target=event_loop.run)
 
         def handler(signum: int, frame: Any) -> None:
-            log.info("Shutting down wifi-manager", signum=signum)
+            log.info('Shutting down wifi-manager', signum=signum)
             wifi_manager.shutdown()
 
             event_loop.quit()
@@ -274,67 +240,8 @@ def main() -> None:
         event_thread.join(1)
 
 
-def _get_arguments() -> dict[str, Any]:
-    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    default_config = f"/etc/effective-range/{APPLICATION_NAME}/{APPLICATION_NAME}.conf"
-    parser.add_argument(
-        "-c",
-        "--config-file",
-        help="configuration file",
-        default=default_config,
-    )
-
-    parser.add_argument("-f", "--log-file", help="log file path")
-    parser.add_argument("-l", "--log-level", help="logging level")
-
-    parser.add_argument(
-        "--api-server-port", help="web server port to listen on", type=int
-    )
-
-    parser.add_argument("--device-role", help="device role")
-    parser.add_argument("--device-hostname", help="hostname pattern")
-
-    parser.add_argument("--wlan-interface", help="preferred wlan interface")
-    parser.add_argument("--wlan-country", help="country code")
-    parser.add_argument("--wlan-disable-power-save", help="disable wlan power save mode",
-                        action="store_true", default=False)
-    parser.add_argument("--wlan-disable-roaming", help="disable wifi roaming",
-                        action="store_true", default=False)
-
-    parser.add_argument("--control-switch-fail-limit", help="mode switching failure limit", type=int)
-    parser.add_argument("--control-switch-fail-command", help="command to execute when reaching failure limit")
-
-    parser.add_argument("--client-timeout", help="client timeout in seconds", type=int)
-    parser.add_argument("--client-restart-delay", help="client restart delay in seconds", type=int)
-
-    parser.add_argument("--hotspot-password", help="hotspot Wi-Fi password")
-    parser.add_argument(
-        "--hotspot-peer-timeout", help="peer timeout in seconds", type=int
-    )
-    parser.add_argument("--hotspot-static-ip", help="hotspot static IP address")
-    parser.add_argument("--hotspot-dhcp-range", help="hotspot DHCP range")
-    parser.add_argument(
-        "--hotspot-startup-delay", help="hotspot startup delay in seconds", type=int
-    )
-
-    args = parser.parse_args()
-    if args.config_file == default_config and not pathlib.Path(args.config_file).exists():
-        def_config = (
-            f"/etc/effective-range/{APPLICATION_NAME}/{APPLICATION_NAME}.conf.default"
-        )
-        args.config_file = def_config
-    return {k: v for k, v in vars(args).items() if v is not None}
-
-
 def _get_resource_root() -> str:
     return str(Path(os.path.dirname(__file__)).parent.absolute())
-
-
-def _update_logging(arguments: dict[str, Any], configuration: dict[str, Any]) -> None:
-    log_level = configuration.get("log_level", "INFO")
-    log_file = configuration.get("log_file")
-    if log_level != "INFO" or log_file != arguments.get("log_file"):
-        setup_logging(APPLICATION_NAME, log_level, log_file, warn_on_overwrite=False)
 
 
 def _init_service(
@@ -351,8 +258,8 @@ def _init_service(
             service.set_force_stop(True)
     else:
         if is_required:
-            raise ValueError(f"Mandatory {service.get_name()} service is not installed")
+            raise ValueError(f'Mandatory {service.get_name()} service is not installed')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -1,52 +1,109 @@
-from setuptools import setup
+from pathlib import Path
+
+from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+
+from arguments import get_argument_parser, APPLICATION_NAME
+
+ROOT_DIR = Path(__file__).resolve().parent
+DEFAULT_CONFIG_REL_PATH = Path('config') / f'{APPLICATION_NAME}.conf.default'
+DEFAULT_CONFIG_PATH = ROOT_DIR / DEFAULT_CONFIG_REL_PATH
+
+
+def generate_default_config() -> None:
+    from common_utility import ConfigLoader
+
+    DEFAULT_CONFIG_PATH.parent.mkdir(parents=True, exist_ok=True)
+
+    argument_parser = get_argument_parser()
+    config_loader = ConfigLoader(DEFAULT_CONFIG_PATH)
+
+    with open(DEFAULT_CONFIG_PATH, 'w') as file:
+        config_loader.dump(argument_parser, file)
+
+
+class BuildPy(build_py):
+    def run(self) -> None:
+        super().run()
+        generate_default_config()
+
+
+class Develop(develop):
+    def run(self) -> None:
+        super().run()
+        generate_default_config()
+
+
+class EggInfo(egg_info):
+    def run(self) -> None:
+        super().run()
+        generate_default_config()
+
+
+commands = {
+    'build_py': BuildPy,
+    'develop': Develop,
+    'egg_info': EggInfo,
+}
+
+extras_require = {
+    'test': [
+        'pytest>=7.0',
+        'pytest-console-scripts',
+        'pytest-coverage'
+    ],
+    'lint': [
+        'flake8',
+        'mypy',
+    ],
+}
+extras_require['dev'] = extras_require['test'] + extras_require['lint']
 
 setup(
-    name="wifi-manager",
-    description="Wi-Fi manager application",
-    long_description="Switching to AP mode if unable to connect to any configured network as a client",
-    author="Ferenc Nandor Janky & Attila Gombos",
-    author_email="info@effective-range.com",
-    maintainer="Ferenc Nandor Janky & Attila Gombos",
-    maintainer_email="info@effective-range.com",
-    packages=[
-        "wifi_utility",
-        "wifi_event",
-        "wifi_dbus",
-        "wifi_config",
-        "wifi_service",
-        "wifi_connection",
-        "wifi_manager",
-    ],
-    scripts=["bin/wifi-manager.py"],
+    name=APPLICATION_NAME,
+    description='Wi-Fi manager application',
+    long_description='Switching to AP mode if unable to connect to any configured network as a client',
+    author='Ferenc Nandor Janky & Attila Gombos',
+    author_email='info@effective-range.com',
+    maintainer='Ferenc Nandor Janky & Attila Gombos',
+    maintainer_email='info@effective-range.com',
+    packages=find_packages(exclude=['tests']),
+    scripts=['bin/wifi-manager.py'],
     data_files=[
         (
-            "config",
+            'config',
             [
-                "config/wifi-manager.conf.default",
-                "config/hostapd.conf.template",
-                "config/dnsmasq.conf.template",
+                str(DEFAULT_CONFIG_REL_PATH),
+                'config/hostapd.conf.template',
+                'config/dnsmasq.conf.template',
             ],
         ),
-        ("templates", ["templates/configuration.html", "templates/operation.html", "templates/execution.html"]),
-        ("static", ["static/style.css"]),
+        ('templates', ['templates/configuration.html', 'templates/operation.html', 'templates/execution.html']),
+        ('static', ['static/style.css']),
     ],
+    cmdclass=commands,
+    extras_require=extras_require,
     use_scm_version=True,
-    setup_requires=["setuptools_scm"],
-    install_requires=[
-        "flask",
-        "waitress",
-        "netifaces",
-        "ping3",
-        "dbus-python",
-        "PyGObject==3.50.0",
-        "pygobject-stubs",
-        "ssdpy",
-        "jinja2",
-        "cysystemd",
-        "parameterized",
-        "gpiozero",
-        "python-context-logger@git+https://github.com/EffectiveRange/python-context-logger.git@latest",
-        "python-common-utility@git+https://github.com/EffectiveRange/python-common-utility.git@latest",
-        "python-systemd-dbus@git+https://github.com/EffectiveRange/python-systemd-dbus.git@latest",
+    setup_requires=[
+        'setuptools_scm',
+        'python-common-utility@git+https://github.com/EffectiveRange/python-common-utility.git@latest'
     ],
+    install_requires=[
+        'flask',
+        'waitress',
+        'netifaces',
+        'ping3',
+        'dbus-python',
+        'PyGObject==3.50.0',
+        'pygobject-stubs',
+        'ssdpy',
+        'jinja2',
+        'cysystemd',
+        'parameterized',
+        'gpiozero',
+        'python-common-utility@git+https://github.com/EffectiveRange/python-common-utility.git@latest',
+        'python-systemd-dbus@git+https://github.com/EffectiveRange/python-systemd-dbus.git@latest',
+    ]
 )
